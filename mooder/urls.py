94ee52b0d9223334c615e5b37bb3e3be7305b0a3
cnskis <admin@sg95.cn>
2024-08-21 17:58:15 +0800

    Update urls.py

--- mooder/urls.py
+++ mooder/urls.py
@@ -17,6 +17,7 @@ from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from archives.views import AnnouncementsListView, AnnouncementsDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
